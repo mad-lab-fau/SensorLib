ca722aa54deaa75556c8af5a96144ccf7528ec4a
Robert Richer <robert.richer@fau.de>
2019-04-09 14:11:16 +0200

    Added Nordic DFU library to "sls-portabiles" module (https://github.com/NordicSemiconductor/Android-DFU-Library) and necessary components: - NilsPodDfuService - NotificationActivity

---
+++ SensorLib/sls-portabiles/src/main/java/de/fau/sensorlib/sensors/dfu/NotificationActivity.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2018 Machine Learning and Data Analytics Lab, Friedrich-Alexander-Universität Erlangen-Nürnberg (FAU).
+ * <p>
+ * This file is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. If you reuse
+ * this code you have to keep or cite this comment.
+ */
+
+package de.fau.sensorlib.sensors.dfu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // If this activity is the root activity of the task, the app is not running
+        /*if (isTaskRoot()) {
+            // Start the app before finishing
+            final Intent intent = new Intent(this, MyActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtras(getIntent().getExtras()); // copy all extras
+            startActivity(intent);
+        }*/
+
+        // Now finish, which will drop you to the activity at which you were at the top of the task stack
+        finish();
+    }
+}
