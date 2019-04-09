9b3c30b49e4869c97a9f1b2f858615e4fe662dc1
Robert Richer <robert.richer@fau.de>
2019-04-09 14:08:36 +0200

    Added SupportsDfu interface for Nordic BLE-SoCs

---
+++ SensorLib/sensorlib/src/main/java/de/fau/sensorlib/sensors/SupportsDfu.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2019 Machine Learning and Data Analytics Lab, Friedrich-Alexander-Universität Erlangen-Nürnberg (FAU).
+ * <p>
+ * This file is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. If you reuse
+ * this code you have to keep or cite this comment.
+ */
+
+package de.fau.sensorlib.sensors;
+
+public interface SupportsDfu {
+
+    void updateFirmware(String filePath);
+
+}
