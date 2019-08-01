8a88ff070b74391e5e65fcc47659ae4c31d57642
Robert Richer <robert.richer@fau.de>
2019-08-01 15:56:49 +0200

    fixed bug in writing sampling rate config

--- SensorLib/sls-portabiles/src/main/java/de/fau/sensorlib/sensors/NilsPodSensor.java
+++ SensorLib/sls-portabiles/src/main/java/de/fau/sensorlib/sensors/NilsPodSensor.java
@@ -412,33 +412,7 @@ public class NilsPodSensor extends AbstractNilsPodSensor implements NilsPodLogga
     public void setCurrentConfig(HashMap<String, Object> configMap) {
         for (String key : configMap.keySet()) {
             Log.d(TAG, "config map: " + key + ", " + configMap.get(key) + ", " + configMap.get(key).getClass());
-            // TODO test thoroughly if this works!!!
             mCurrentConfigMap.put(key, configMap.get(key));
-
-            /*switch (key) {
-                case KEY_SAMPLING_RATE:
-                    String sr = (String) configMap.get(key);
-                    samplingRate = sAvailableSamplingRates.get(sr);
-                    break;
-                case KEY_HARDWARE_SENSORS:
-                    sensors = (ArrayList<HardwareSensor>) configMap.get(key);
-                    break;
-                case KEY_SENSOR_POSITION:
-                    sensorPosition = (NilsPodSensorPosition) configMap.get(key);
-                    break;
-                case KEY_SYNC_GROUP:
-                    syncGroup = (NilsPodSyncGroup) configMap.get(key);
-                    break;
-                case KEY_SYNC_ROLE:
-                    syncRole = (NilsPodSyncRole) configMap.get(key);
-                    break;
-                case KEY_OPERATION_MODE:
-                    operationMode = (NilsPodOperationMode) configMap.get(key);
-                    break;
-                case KEY_MOTION_INTERRUPT:
-                    interrupt = (NilsPodMotionInterrupt) configMap.get(key);
-                    break;
-            }*/
         }
     }
 
@@ -452,6 +426,11 @@ public class NilsPodSensor extends AbstractNilsPodSensor implements NilsPodLogga
         NilsPodSyncRole syncRole = (NilsPodSyncRole) mCurrentConfigMap.get(KEY_SYNC_ROLE);
         NilsPodOperationMode operationMode = (NilsPodOperationMode) mCurrentConfigMap.get(KEY_OPERATION_MODE);
         NilsPodMotionInterrupt interrupt = (NilsPodMotionInterrupt) mCurrentConfigMap.get(KEY_MOTION_INTERRUPT);
+
+        String sr = (String) mCurrentConfigMap.get(KEY_SAMPLING_RATE);
+        if (sr != null) {
+            samplingRate = sAvailableSamplingRates.get(sr);
+        }
 
         try {
             writeSamplingRateConfig(samplingRate);
